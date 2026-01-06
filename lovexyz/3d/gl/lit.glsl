d50fa134b2e7d5bb2b65a1318bbbdc90fb724b30
Zellicious <kenzierasyadalfarezel@gmail.com>
2026-01-06 12:24:44 +0800

    update lovexyz/3d/gl/lit.glsl

--- lovexyz/3d/gl/lit.glsl
+++ lovexyz/3d/gl/lit.glsl
@@ -12,57 +12,59 @@ uniform number u_Shininess;
 uniform number u_Specular;
 uniform vec2 u_ShadowMapSize;
 
+uniform bool shadowEnabled;
+uniform bool specularEnabled;
+uniform bool diffuseEnabled;
+
 const float shadowDetail = 2.0;
 
 float rand(vec2 co)
 {
-    return fract(sin(dot(co, vec2(127856.9898,77567.233))) * 43.5453);
+  return fract(sin(dot(co, vec2(127856.9898,77567.233))) * 43.5453);
 }
 
 float linearizeDepth(float d)
 {
-    float near = 0.1;
-    float far  = 500.0;
-    return (2.0 * near) / (far + near - d * (far - near));
+  float near = 0.1;
+  float far  = 500.0;
+  return (2.0 * near) / (far + near - d * (far - near));
 }
 
 highp float sampleShadow(vec4 lightSpacePos,vec3 N, vec3 L)
 {
-    vec3 proj = lightSpacePos.xyz / lightSpacePos.w;
-    vec2 uv = proj.xy * 0.5 + 0.5;
+  vec3 proj = lightSpacePos.xyz / lightSpacePos.w;
+  vec2 uv = proj.xy * 0.5 + 0.5;
 
-    if (uv.x <= 0.0 || uv.x >= 1.0 || uv.y <= 0.0 || uv.y >= 1.0)
-        return 1.0;
+  if (uv.x <= 0.0 || uv.x >= 1.0 || uv.y <= 0.0 || uv.y >= 1.0)
+    return 1.0;
         
-    highp float currentDepth = proj.z;
+  highp float currentDepth = proj.z;
 
-    highp float bias = max(0.0005 * (1.0 - dot(N, L)), 0.0003);
-    highp float shadow = 0.0;
+  highp float bias = max(0.0005 * (1.0 - dot(N, L)), 0.0003);
+  highp float shadow = 0.0;
 
-    highp vec2 texelSize = 1.0 / u_ShadowMapSize / shadowDetail;
-    highp float angle = rand(gl_FragCoord.xy) * 6.2831853;
-    mat2 rot = mat2(cos(angle), -sin(angle),
+  highp vec2 texelSize = 1.0 / u_ShadowMapSize / shadowDetail;
+  highp float angle = rand(gl_FragCoord.xy) * 6.2831853;
+  mat2 rot = mat2(cos(angle), -sin(angle),
                             sin(angle),  cos(angle));
 
-    float weightSum = 0.0;
+  float weightSum = 0.0;
     
-    for (int x = -int(shadowDetail/2.0); x <= int(shadowDetail/2.0); x++)
+  for (int x = -int(shadowDetail/2.0); x <= int(shadowDetail/2.0); x++)
+  {
+    for (int y = -int(shadowDetail/2.0); y <= int(shadowDetail/2.0); y++)
     {
-        for (int y = -int(shadowDetail/2.0); y <= int(shadowDetail/2.0); y++)
-        {
-            vec2 p = vec2(float(x), float(y));
-    
-            vec2 offset = rot * p * texelSize;
-    
-            highp float depth = Texel(shadowMap, uv + offset).r;
-            shadow += ((currentDepth - bias > depth) ? 0.0 : 1.0);
-            weightSum += 1.0;
-        }
+      vec2 p = vec2(float(x), float(y));
+      vec2 offset = rot * p * texelSize;
+      highp float depth = Texel(shadowMap, uv + offset).r;
+      shadow += ((currentDepth - bias > depth) ? 0.0 : 1.0);
+      weightSum += 1.0;
     }
+  }
     
-    shadow /= weightSum;
+  shadow /= weightSum;
 
-    return shadow;
+  return shadow;
 }
 
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
@@ -84,15 +86,26 @@ vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
     float diff = NoL;
     vec3 H = normalize(L + V);
     float spec = pow(min(max(dot(N,H),0.0),1.0), u_Shininess);
-
-    spec *= shadowStrength;
-    diff *= shadowStrength;
+    
+    if (shadowEnabled)
+    {
+      spec *= shadowStrength;
+      diff *= shadowStrength;
+    }
     
     float lighting = (u_Ambient + diff);
     // base texture color
     vec4 texColor = Texel(tex, texture_coords) * color;
-    texColor.rgb *= lighting;
-    texColor.rgb += vec3(spec * u_Specular);
     
+    
+    
+    if (diffuseEnabled)
+    {
+      texColor.rgb *= lighting;
+    }
+    if (specularEnabled)
+    {
+      texColor.rgb += vec3(spec * u_Specular);
+    }
     return vec4(texColor.rgb, 1.0);
 }
