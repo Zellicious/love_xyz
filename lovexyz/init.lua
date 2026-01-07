bc0e86cbab56cec78b3fb704546d3cb8c715d48d
Zellicious <kenzierasyadalfarezel@gmail.com>
2026-01-07 16:08:25 +0800

    fixed stuff

--- lovexyz/init.lua
+++ lovexyz/init.lua
@@ -11,7 +11,7 @@ obj = require(basePathName..".3d.lua.obj")
 engine = require(basePathName..".3d.engine")
 engine.path = basePathName
 
-engine._VERSION = "lovexyz 1.1.7"
+engine._VERSION = "lovexyz 1.1.9"
 print(engine._VERSION)
 
 local engine = engine
